4fd8f12d0e8030d10724535e8d26b94722a121c6
Can Berk Durmus <canberkdurmus@gmail.com>
2021-05-23 17:28:16 +0300

    Add lexical analyzer

---
+++ src/lexical_analyzer.py
@@ -0,0 +1,23 @@
+from lex_line import LexLine
+
+
+class LexicalAnalyzer:
+    def __init__(self, filename):
+        source_file = open(filename, 'r')
+        self.tokens = []
+
+        lines = source_file.readlines()
+
+        for line in lines:
+            line = line.replace('\n', '')
+            if len(line) == 0 or line[0] == '#':
+                continue
+            lex = LexLine(line, debug=False)
+            lex.print_classified()
+
+            for token in lex.tokens:
+                self.tokens.append(token)
+
+            self.tokens.append(('symbol', '$'))
+
+        print(self.tokens)
