b6bb6babee4956eb4f9ba327f6bb1b5575a60e40
Can Berk Durmus <canberkdurmus@gmail.com>
2021-06-13 09:33:47 +0300

    Separate statement types while parsing and add to parse tree accordingly

--- src/parse_tree.py
+++ src/parse_tree.py
@@ -8,6 +8,12 @@ class Node:
             return
         self.children.append(leaf)
 
+    def add_leaves(self, leaves):
+        for leaf in leaves:
+            if leaf == '$':
+                del leaves[leaves.index(leaf)]
+        self.children = self.children + leaves
+
     def parse_decl(self, tokens):
         node = Node([])
         node.add_leaf(tokens)
@@ -25,25 +31,39 @@ class Node:
         node.add_leaf(tokens[2])  # (
         i += 1
         tmp_node = Node([])
-        tmp_node.children = tmp_node.children + parameters
+        tmp_node.add_leaves(parameters)
+        i += len(parameters)
         node.add_leaf(tmp_node)  # parameters
         node.add_leaf(tokens[i])  # )
         i += 1
         node.add_leaf(tokens[i])  # {
         i += 1
-        print('body', body)
         tmp_node = Node(body)
-        # tmp_node.children = tmp_node.children + body
         node.add_leaf(tmp_node)  # function body
-        i += len(body)
+        i += len(body) + 1
         node.add_leaf(tokens[i])  # }
         i += 1
         self.add_leaf(node)
         print('Function: ', tokens)
+        print(node.children)
         print(len(self.children))
 
-    def parse_stat(self, tokens, statement_type):
-        print('Statement', statement_type, tokens)
+    def parse_assignstat(self, tokens):
+        print('Statement assignment', tokens)
+        node = Node([])
+        node.add_leaves(tokens)
+        self.add_leaf(node)
+        print(node.children)
+        print(len(self.children))
+
+    def parse_loopstat(self, tokens, boolexp, body):
+        ...
+
+    def parse_ifstat(self, tokens, boolexp, if_body, else_body):
+        ...
+
+    def parse_returnstat(self, tokens):
+        ...
 
     def parse_root(self, tokens):
         if len(tokens) == 0:
@@ -74,7 +94,7 @@ class Node:
                 while token != '$':
                     tmp_tokens.append(token)
                     index, token, token_type = self.get_next_token(index, tokens)
-                self.parse_stat(tmp_tokens, 'assignment')
+                self.parse_assignstat(tmp_tokens)
 
             elif token == 'fun':
                 # Function (multi line)
@@ -94,7 +114,7 @@ class Node:
                     elif token == ')':
                         parenthesis_stack -= 1
                     tmp_tokens.append(token)
-                    tmp_parameters.append((token_type, token))
+                    tmp_parameters.append(token)
                     index, token, token_type = self.get_next_token(index, tokens)
                 del tmp_parameters[-1]
 
@@ -124,19 +144,23 @@ class Node:
                 index, token, token_type = self.get_next_token(index, tokens)
                 tmp_tokens.append(token)  # Add '('
                 index, token, token_type = self.get_next_token(index, tokens)
-                parenthesis_stack = 1
 
+                boolexp = []
+                parenthesis_stack = 1
                 while parenthesis_stack > 0:
                     if token == '(':
                         parenthesis_stack += 1
                     elif token == ')':
                         parenthesis_stack -= 1
                     tmp_tokens.append(token)
+                    boolexp.append((token_type, token))
                     index, token, token_type = self.get_next_token(index, tokens)
+                del boolexp[-1]
 
                 tmp_tokens.append(token)  # Add '{'
                 index, token, token_type = self.get_next_token(index, tokens)
 
+                loop_body = []
                 braces_stack = 1
                 while braces_stack > 0:
                     if token == '{':
@@ -144,31 +168,36 @@ class Node:
                     elif token == '}':
                         braces_stack -= 1
                     tmp_tokens.append(token)
+                    loop_body.append((token_type, token))
                     index, token, token_type = self.get_next_token(index, tokens)
+                del loop_body[-1]
 
-                self.parse_stat(tmp_tokens, token_type)
+                self.parse_loopstat(tmp_tokens, boolexp, loop_body)
 
             elif token == 'if':
                 # If Statement (multi line)
                 tmp_tokens = []
-                token_type = token
                 tmp_tokens.append(token)  # Add 'if'
                 index, token, token_type = self.get_next_token(index, tokens)
                 tmp_tokens.append(token)  # Add '('
                 index, token, token_type = self.get_next_token(index, tokens)
-                parenthesis_stack = 1
 
+                conditional = []
+                parenthesis_stack = 1
                 while parenthesis_stack > 0:
                     if token == '(':
                         parenthesis_stack += 1
                     elif token == ')':
                         parenthesis_stack -= 1
                     tmp_tokens.append(token)
+                    conditional.append((token_type, token))
                     index, token, token_type = self.get_next_token(index, tokens)
+                del conditional[-1]
 
                 tmp_tokens.append(token)  # Add '{'
                 index, token, token_type = self.get_next_token(index, tokens)
 
+                if_body = []
                 braces_stack = 1
                 while braces_stack > 0:
                     if token == '{':
@@ -176,8 +205,11 @@ class Node:
                     elif token == '}':
                         braces_stack -= 1
                     tmp_tokens.append(token)
+                    if_body.append((token_type, token))
                     index, token, token_type = self.get_next_token(index, tokens)
+                del if_body[-1]
 
+                else_body = []
                 if token == 'else':
                     tmp_tokens.append(token)  # Add 'else'
                     index, token, token_type = self.get_next_token(index, tokens)
@@ -190,9 +222,11 @@ class Node:
                         elif token == '}':
                             braces_stack -= 1
                         tmp_tokens.append(token)
+                        else_body.append((token_type, token))
                         index, token, token_type = self.get_next_token(index, tokens)
+                    del else_body[-1]
 
-                self.parse_stat(tmp_tokens, token_type)
+                self.parse_ifstat(tmp_tokens, conditional, if_body, else_body)
 
             elif token == 'return':
                 # Return Statement (one line)
@@ -200,7 +234,7 @@ class Node:
                 while token != '$':
                     tmp_tokens.append(token)
                     index, token, token_type = self.get_next_token(index, tokens)
-                self.parse_stat(tmp_tokens, 'return')
+                self.parse_returnstat(tmp_tokens)
 
             else:
                 # Raise error
